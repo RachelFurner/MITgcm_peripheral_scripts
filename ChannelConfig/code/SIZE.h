38e6ac9d9860b8009c99e7e20f31c88eea749ca5
Rachel Furner <rfurner@jasmin-sci1.ceda.ac.uk>
2020-07-28 14:14:17 +0100

    updated to run on 96 procs

--- ChannelConfig/code/SIZE.h
+++ ChannelConfig/code/SIZE.h
@@ -45,13 +45,13 @@ C     Nr  :: No. points in Z for full process domain.
       INTEGER Nr
       PARAMETER (
      &           sNx =  40,
-     &           sNy =  51,
+     &           sNy =  50,
      &           OLx =   4,
      &           OLy =   4,
      &           nSx =   1,
      &           nSy =   1,
-     &           nPx =   1,
-     &           nPy =   1,
+     &           nPx =  24,
+     &           nPy =   4,
      &           Nx  = sNx*nSx*nPx,
      &           Ny  = sNy*nSy*nPy,
      &           Nr  =   38 )
